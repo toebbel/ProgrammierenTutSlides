6672c7823ef092a266ab8fdd69dd73889234e33a
= <tobstu@gmail.com>
2012-10-28 15:58:12 +0100

    initial

---
+++ src/clear.bat
@@ -0,0 +1,16 @@
+echo off
+echo Loesche alle LaTex-Hilfsdateien. Bestätigen mit Enter.
+Pause
+del *.toc
+del *.synctex.gz
+del *.snm
+del *.out
+del *.nav
+del *.log
+del *.aux
+del *.synctex.gz(busy)
+del *.vbr
+del *.bcf
+del *.run.xml
+del *.vrb
+del *.dvi
